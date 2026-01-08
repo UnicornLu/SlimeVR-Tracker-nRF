44bb0e8bcec650e1767539ccd0313158ef3d614d
UnicornLu <unicornlu380@gmail.com>
2026-01-08 17:02:24 -0500

    Update power.c

--- src/system/power.c
+++ src/system/power.c
@@ -26,19 +26,6 @@
 
 static uint32_t *dbl_reset_mem __attribute__((unused)) = ((uint32_t *)DFU_DBL_RESET_MEM); // retained
 
-#if ADAFRUIT_BOOTLOADER
-static inline void uf2_clear_dfu_triggers(void)
-{
-    /* 清 UF2 double-tap RAM magic */
-    *(volatile uint32_t *)DFU_DBL_RESET_MEM = 0;
-
-    /* 清 UF2 常用的 GPREGRET 触发位 */
-    NRF_POWER->GPREGRET = 0;
-#ifdef NRF_POWER_GPREGRET2_Msk
-    NRF_POWER->GPREGRET2 = 0;
-#endif
-}
-#endif
 
 enum sys_regulator {
 	SYS_REGULATOR_DCDC,
